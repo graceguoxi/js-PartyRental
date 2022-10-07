99577e617c1735b780d361ba2b76ba98e0bb1acf
Grace Guo <guoxi2005@hotmail.com>
2022-10-07 23:30:02 +1100

    07/10

--- index.js
+++ index.js
@@ -1,42 +1,3 @@
-// const productList = rawdata.filter(item => item.productMedia && item.productMedia.length > 0)
-
-// console.log('products', productList);
-
-// let listElement = document.querySelector('#prodList');
-// let templates = ''
-
-// // for(let product of productList)
-
-// productList.forEach(product => {
-// 	const viewTemplate = `
-// 		<div class="col-3">
-// 			<a href="./detail.html?prodId=${product.prodId}">
-// 				<div class="card">
-// 					<img src="https://storage.googleapis.com/luxe_media/wwwroot/${product.productMedia[0].url}" class="card-img-top" alt="...">
-// 					<div class="card-body">
-// 						<h5 class="card-title">${product.title}</h5>
-// 						<p class="card-text">$ ${product.price}</p>
-// 					</div>
-// 				</div>
-// 			</a>
-// 		</div>
-// 	`
-// 	templates += viewTemplate
-
-//   // let col = document.createElement("div");
-//   // col.setAttribute('class', 'col-3')
-//   // let card = document.createElement("div");
-//   // card.setAttribute('class', 'card');
-//   // let img = document.createElement("img");
-//   // img.setAttribute('src', "https://storage.googleapis.com/luxe_media/wwwroot/Images/ProductImages/21b2890a-21dc-4d62-b628-3df10a6507c0");
-//   // card.appendChild(img);
-//   // col.appendChild(card);
-//   // list.appendChild(col);
-// })
-
-// listElement.innerHTML = templates
-
-// console.log('prod', prodList)
 
 
 const prodCategoryList = rawdata[0].prodType.productCategory
@@ -53,33 +14,90 @@ for (let i of prodCategoryList) {
 
 allcategorys.innerHTML = category
 
-const productList2 = rawdata.filter(item =>item.productMedia && item.productMedia.length >0)
 
-// console.log('new',productList2)
+const productList1 = rawdata.filter(item =>item.productMedia && item.productMedia.length >0)
 
-let listElement2 = document.querySelector('#prodList')
-let templates2 = ''
+function filterCategory () {
+	// console.log("aa")
+	let select = document.getElementById('select')
+	let value = select.value
+	// console.log(value)
 
-productList2.forEach(product => {
-// for (let product of productList2) {
-  
+	const selectedProd = rawdata.filter(function(item) {
 
-	let viewTemplate2 = `
-		<div class="col-3">
-			<a href="./detail/detail.html?prodId=${product.prodId}">
-				<div class="card">
-					<img src="https://storage.googleapis.com/luxe_media/wwwroot/${product.productMedia[0].url}" class="card-img-top" alt="...">
-					<div class="card-body">
-						<h5 class="card-title">${product.title}</h5>
-						<p class="card-text">$ ${product.price}</p>
-					</div>
-				</div>
-			</a>
-		</div>
-	`
-	templates2 += viewTemplate2
+	  return item.categoryId == value && item.productMedia && item.productMedia.length >0
+		 	
+	})
+	// console.log('1111',selectedProd)
+	list(selectedProd)
+	
 }
-)
+
+function filterPrice() {
+  // console.log('bbb')
+
+	let selectedPrice = document.querySelector
+	('#price-select')
+	let priceValue = selectedPrice.value
+	// console.log('8888',priceValue)
+	let select = document.getElementById('select')
+	let value = select.value
+  console.log('all' ,value)
+
+  const selePriceProd = rawdata.filter(function(item) {
+		let commonFilter = item.productMedia && item.productMedia.length >0
+		if(value !== '0') {
+      commonFilter = item.categoryId == value && commonFilter
+		}
+		if (priceValue === '100') {
+			// console.log ('100',100)
+      return item.price <= 100 && commonFilter
+		}
+		if (priceValue == '500' ) {
+			// console.log(500)
+			return item.price > 100 && item.price <= 500 && commonFilter
+		}
+		if(priceValue === '1000') {
+			return item.price > 500 && item.price <= 1000 && commonFilter
+		}
+		if(priceValue === '>1000') {
+			return item.price >1000 && commonFilter
+		}
+		return commonFilter
+	})
+	// console.log(selePriceProd)
+	list(selePriceProd)
+}
 
 
-listElement2.innerHTML = templates2
+function list (productList) {
+
+	let listElement = document.querySelector('#prodList')
+	let templates = ''
+
+	productList.forEach(product => {
+	// for (let product of productList) {
+		
+
+		let viewTemplate = `
+			<div class="col-3">
+				<a href="./detail/detail.html?prodId=${product.prodId}">
+					<div class="card">
+						<img src="https://storage.googleapis.com/luxe_media/wwwroot/${product.productMedia[0].url}" class="card-img-top" alt="...">
+						<div class="card-body">
+							<h5 class="card-title">${product.title}</h5>
+							<p class="card-text">$ ${product.price}</p>
+						</div>
+					</div>
+				</a>
+			</div>
+		`
+		templates += viewTemplate
+	}
+	)
+
+
+	listElement.innerHTML = templates
+}
+
+list(productList1);
