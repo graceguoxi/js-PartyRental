d87647293d82dd0eb044737f9afdfb698330050f
Grace Guo <guoxi2005@hotmail.com>
2022-10-18 21:53:21 +1100

    finish

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
 
-const prodCategoryList = rawdata[0].prodType.productCategory
-
-let allcategorys = document.querySelector('#select')
-
-let category = `<option value="0">ALL Hires</option>`
-
-for (let i of prodCategoryList) {
-	category += `
-		<option value="${i.categoryId}">${i.categoryName}</option>
-	`
-}
-
-allcategorys.innerHTML = category
+// 1. get the product categories and render the category select list
 
 const url = new URL(window.location.href)
-console.log('search', url.search)
 const params = new URLSearchParams(url.search)
-console.log('params', params.toString())
-const productList1 = rawdata.filter(item =>item.productMedia && item.productMedia.length >0)
-
-// function filterCategory () {
-// 	// console.log("aa")
-// 	let select = document.getElementById('select')
-// 	let value = select.value
-// 	// console.log(value)
-
-// 	const selectedProd = rawdata.filter(function(item) {
-
-// 	  return item.categoryId == value && item.productMedia && item.productMedia.length >0
-		 	
-// 	})
-// 	// console.log('1111',selectedProd)
-// 	list(selectedProd)
-	
-// }
+let selectedCategory = document.getElementById('category-select')
 let selectedPrice = document.querySelector('#price-select')
-let selectedCategory = document.getElementById('select')
 
-function filterAll(clicked_id) {
-	if( clicked_id == 'ascend' ) {
-		params.set('sort','asc')
-	}else if ( clicked_id == 'decend' ) {
-		params.set('sort','desc')
-	}else if ( clicked_id == 'reset'){
-		params.delete('categoryId')
-		params.delete('priceRange')
-		params.delete('sort')
-	}else if( clicked_id == 'select') {
-    params.set('categoryId',selectedCategory.value)
-	}else if(clicked_id == 'price-select') {
-		params.set('priceRange',selectedPrice.value)
+renderCategoryList()
+executeFilter()
+
+function renderCategoryList() {
+	const prodCategoryList = rawdata[0].prodType.productCategory
+	let category = `<option value="0">All hires</option>`
+
+	for(let i of prodCategoryList) {
+  	category += `
+	  	<option value="${i.categoryId}">${i.categoryName}</option>
+		`
 	}
 
+	selectedCategory.innerHTML = category
+}
 
+
+// 2. update/delete the url search params based on the users select/click,
+// then navigate to the updated url
+
+function filterAll(clicked_id) {
+  if(clicked_id == 'category-select'){
+		params.set('categoryId',selectedCategory.value)
+	}else if(clicked_id == 'price-select') {
+		params.set('priceRange',selectedPrice.value)
+	}else if(clicked_id == 'ascend') {
+		params.set('sort','asc')
+	}else if(clicked_id == 'decend') {
+		params.set('sort','desc')
+	} else if(clicked_id == 'reset') {
+		params.delete('categoryId')
+		params.delete('priceRange')
+		params.delete('sort') 
+	}
 	window.location.href = `${window.location.pathname}?${params.toString()}`
 }
 
+
+// 3. based on the filters and sort, filter out the products and render products
 function executeFilter() {
 	
 	const categoryValue = params.get('categoryId') || "0"
@@ -70,28 +59,13 @@ function executeFilter() {
 	}
 
 	const sort = params.get('sort')
-	
-
-  // console.log('bbb')
-	// console.log(clicked_id)
-
-	// let selectedPrice = document.querySelector('#price-select')
-	// let priceValue = selectedPrice.value
-	// console.log('8888',priceValue)
-	// let selectedCategory = document.getElementById('select')
-	// let categoryValue = selectedCategory.value
-  console.log('all' ,categoryValue)
+	// console.log('all' ,categoryValue)
 
   let seleProd = rawdata.filter((item) => {
 		// let commonFilter = item.productMedia && item.productMedia.length >0
-
-		let commonFilter = (item.productMedia && item.productMedia.length > 0) ?
+    let commonFilter = (item.productMedia && item.productMedia.length > 0) ?
 		item:null
-		// if(item.productMedia && item.productMedia.length > 0) commonFilter = item
-
-		// console.log(commonFilter)
-
-
+		
 		if(categoryValue !== '0') {
       commonFilter = item.categoryId == categoryValue && commonFilter
 			// console.log(commonFilter)
@@ -113,7 +87,7 @@ function executeFilter() {
 		}
 		return commonFilter
 	})
-	console.log(seleProd)
+	// console.log(seleProd)
   
 	if( sort == 'asc' ) {
 		seleProd.sort((a,b) => a.price - b.price)
@@ -150,40 +124,12 @@ function list(productList) {
 		templates += viewTemplate
 	}
 	)
-
-
 	listElement.innerHTML = templates
 }
 
-executeFilter();
 
-// list(productList1);
 
-// let ascend = document.querySelector('#ascend');
-// let decend = document.querySelector('#decend');
-// let reset = document.querySelector('#reset');
 
-// console.log('as',ascend);
-
-// function ascending () {
-// 	let products = rawdata.filter(item =>item.productMedia && item.productMedia.length >0)
-
-// 	products.sort((a,b) => a.price - b.price)
-// 	list(products);
-// }
-
-// function decending () {
-// 	let products = rawdata.filter(item =>item.productMedia && item.productMedia.length >0)
-
-// 	products.sort((a,b) => b.price - a.price)
-// 	list(products);
-// }
-
-// function resetItem () {
-// 	let products = rawdata.filter(item =>item.productMedia && item.productMedia.length >0)
-
-// 	list(products);
-// }
 
 
 	
